9b1e8cbb588d1001ae06dc4d100466a7b04e502b
Ankit Sinha <anksinha@microsoft.com>
2025-10-26 07:11:56 +0000

    Convert misc.conf.yaml shell steps to expanded command blocks; make package installer idempotent
    - Replace compact list entries in misc.conf.yaml with full mapping (command, description, stdin/stdout/stderr)
    - Add existence checks in scripts/install-packages.sh for fzf, zsh-syntax-highlighting and zsh-autosuggestions to skip already-installed items
    - Preserve package installation for zoxide/bat and improve output messages

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -1,16 +1,31 @@
+#!/bin/bash
+set -e
+
 # install zoxide batcat
 echo "Installing zoxide and bat..."
 sudo apt update && sudo apt install -y zoxide bat
 
 # install fzf
-echo "Installing fzf..."
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --key-bindings --completion --no-update-rc
+if [ ! -d ~/.fzf ]; then
+    echo "Installing fzf..."
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install --key-bindings --completion --no-update-rc
+else
+    echo "fzf already installed, skipping..."
+fi
 
 # install zsh-syntax-highlighting
-echo "Installing zsh-syntax-highlighting plugin for Oh My Zsh..."
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
+    echo "Installing zsh-syntax-highlighting plugin for Oh My Zsh..."
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+else
+    echo "zsh-syntax-highlighting already installed, skipping..."
+fi
 
 # install zsh-autosuggestions
-echo "Installing zsh-autosuggestions plugin for Oh My Zsh..."
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+    echo "Installing zsh-autosuggestions plugin for Oh My Zsh..."
+    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+else
+    echo "zsh-autosuggestions already installed, skipping..."
+fi
